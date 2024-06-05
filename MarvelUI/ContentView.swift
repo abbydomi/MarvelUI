e0dc656ad082ab48dbe9061a77c0635fe440efd1
Abby Dominguez <abbytorade@icloud.com>
2024-06-05 14:16:59 +0200

    Initial Commit

---
+++ MarvelUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarvelUI
+//
+//  Created by Abby Dominguez on 5/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarvelUI/MarvelUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelUIApp.swift
+//  MarvelUI
+//
+//  Created by Abby Dominguez on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
